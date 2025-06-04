4bf4bdcc4d439298346e2ed24bca1f359cc4bd58
Maksim <maksimpv1@bk.ru>
2025-06-04 17:50:47 +0300

    ru

--- src/app/components/experience/Experience.tsx
+++ src/app/components/experience/Experience.tsx
@@ -15,8 +15,9 @@ export const Experience = () => {
                         11.2024 - 01.2025
                     </SC.ExpName>
                     <SC.ExpText>
-                     - Working on a project with the Next js framework;<br/>
-                     - Collaborated with UX/UI team to plan and coordinate user-focused experience improvements.
+                     - Работа на реальном проекте Next js;<br/>
+                    - Работа со стеком: React, Typescript, Next.js, Redux Toolkit, styled-components.<br/>
+                    -  Сотрудничал с командой UX/UI для разработки улучшений, ориентированных на пользователя.
                     </SC.ExpText>
                 </SC.ExperiensBox>
                 <SC.ExperiensBox>
@@ -25,9 +26,9 @@ export const Experience = () => {
                         03.2024 - 10.2024
                     </SC.ExpName>
                     <SC.ExpText>                    
-                        - Working on a project with the Next js framework;<br/>
-                        - Partnered with UX/UI and backend teams to align design concepts with technical implementation.;<br/>
-                        - Working with the stack: Vite, React, Typescript, Next.js, Redux Toolkit, React-i18next, React Big Calendar, date-fns, MUI.<br/>
+                        - Работа на реальном проекте Next js;<br/>
+                        - Работа со стеком: Vite, React, Typescript, Next.js, Redux Toolkit, React-i18next, React Big Calendar, date-fns, MUI.<br/>
+                        - Сотрудничал с UX / UI и серверными командами для согласования концепций дизайна с технической реализацией.
                     </SC.ExpText>
                 </SC.ExperiensBox>
                 <SC.ExperiensBox>
@@ -36,10 +37,11 @@ export const Experience = () => {
                       09.2021 - Present
                     </SC.ExpName>
                     <SC.ExpText>
-                     - "Managed project creation from the design phase through to deployment;"<br/>
-                     - "Collaborated in team utilizing Git for version control and streamlined development workflows;"<br/>
-                     - "Practical expertise in drafting various types of technical documentation;"<br/>
-                     - "Developed, configured, and maintained the organization’s website, enhancing functionality and user experience."
+                     - Руководил созданием проекта от этапа проектирования до развертывания;<br/>
+                     - Сотрудничал в команде, используя Git для контроля версий и оптимизации рабочих процессов разработки;<br/>
+                     - Практический опыт в составлении различных видов технической документации.<br/>
+                     - Разработал, настроил и поддерживал веб-сайт организации, улучшив функциональность и удобство для пользователей.<br/>
+                     - Работа со стеком: React, Typescript, Next.js, Redux Toolkit, styled-components.
                     </SC.ExpText>
                 </SC.ExperiensBox>  
                 <SC.BtnBox>
